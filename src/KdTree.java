6c44a90fc90f1322950a9056999ceaf3f0b1c518
TienDM <TienDM@TienDM>
2017-12-05 02:10:33 +0700

    xzczxc

--- src/KdTree.java
+++ src/KdTree.java
@@ -1,10 +1,45 @@
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
 
 public class KdTree {
-
-	public void insert(Point2D p) {
-		// TODO Auto-generated method stub
-		
+	
+	public KdTree() {
 	}
 
+	public boolean isEmpty() { // is the set empty?
+		return true;
+	}
+
+	public int size() { // number of points in the set
+	}
+
+	public void insert(Point2D p) { // add the point to the set (if it is not
+									// already in the set)
+	}
+
+	public boolean contains(Point2D p) { // does the set contain point p?
+	}
+
+	public void draw() { // draw all points to standard draw
+	}
+
+	public Iterable<Point2D> range(RectHV rect) { // all points that are inside
+													// the rectangle (or on the
+													// boundary)
+	}
+
+	public Point2D nearest(Point2D p) { // a nearest neighbor in the set to
+										// point p; null if the set is empty
+	}
+
+	public static void main(String[] args) { // unit testing of the methods
+												// (optional)
+	}
+
+	private static class Node {
+		private Point2D p; // the point
+		private RectHV rect; // the axis-aligned rectangle corresponding to this node
+		private Node lb; // the left/bottom subtree
+		private Node rt; // the right/top subtree
+	}
 }
--- src/PointSET.java
+++ src/PointSET.java
@@ -1,5 +1,8 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
 
 public class PointSET {
+	
 	public PointSET() { // construct an empty set of points
 	}
 
@@ -10,7 +13,8 @@ public class PointSET {
 	public int size() { // number of points in the set
 	}
 
-	public void insert(Point2D p) { // add the point to the set (if it is not already in the set)
+	public void insert(Point2D p) { // add the point to the set (if it is not
+									// already in the set)
 	}
 
 	public boolean contains(Point2D p) { // does the set contain point p?
@@ -19,12 +23,23 @@ public class PointSET {
 	public void draw() { // draw all points to standard draw
 	}
 
-	public Iterable<Point2D> range(RectHV rect) { // all points that are inside the rectangle (or on the boundary)
+	public Iterable<Point2D> range(RectHV rect) { // all points that are inside
+													// the rectangle (or on the
+													// boundary)
 	}
 
-	public Point2D nearest(Point2D p) { // a nearest neighbor in the set to point p; null if the set is empty
+	public Point2D nearest(Point2D p) { // a nearest neighbor in the set to
+										// point p; null if the set is empty
 	}
 
-	public static void main(String[] args) { // unit testing of the methods (optional)
+	public static void main(String[] args) { // unit testing of the methods
+												// (optional)
+	}
+
+	private static class Node {
+		private Point2D p; // the point
+		private RectHV rect; // the axis-aligned rectangle corresponding to this node
+		private Node lb; // the left/bottom subtree
+		private Node rt; // the right/top subtree
 	}
 }
